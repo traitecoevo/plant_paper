9a05db0269804263aab3d19eaa347b40a3307e81
Rich FitzJohn <rich.fitzjohn@gmail.com>
2015-05-15 13:40:13 +1000

    First draft of fitness landscape figure
    For #11

---
+++ R/figures_fitness.R
@@ -0,0 +1,74 @@
+make_fitness_data <- function() {
+  p0 <- ebt_base_parameters()
+  p0$control$equilibrium_nsteps <- 30
+  p0$control$equilibrium_solver_name <- "hybrid"
+  p0$disturbance_mean_interval <- 30.0
+
+  bounds <- bounds_infinite("lma")
+
+  bounds <- viable_fitness(bounds, p0)
+  bounds <- check_bounds(bounds)
+
+  ## Communities:
+  communities <-
+    list(first      = 0.10,           # first individual
+         attractor1 = 0.08,           # first branching point
+         second1    = c(0.08, 0.22),  # ~= the max fitness
+         second2    = c(0.08, 0.25),  # ~= the max fitness
+         second3    = c(0.08, 0.30),  # ~= the max fitness
+         attractor2 = c(0.08, 0.267)) # stable
+
+  lma_mutant <- seq_log_range(bounds, 101)
+
+  lma <- sort(c(unique(unlist(communities)), lma_mutant))
+
+  ## TODO: First strategy should be at 0.1 so that we see an example
+  ## that is out of equilibrium...
+
+  f <- function(x, p) {
+    equilibrium_seed_rain(expand_parameters(trait_matrix(x, "lma"), p, FALSE))
+  }
+  pp <- lapply(communities, f, p0)
+  ww <- lapply(pp, fitness_landscape, trait_matrix=trait_matrix(lma, "lma"))
+
+  list(communities=communities,
+       lma=lma,
+       p=pp,
+       w=ww)
+}
+
+figure_fitness_landscape <- function(data) {
+  communities <- data$communities
+  lma <- data$lma
+  w <- data$w
+
+  ## Start the plots:
+  xlim <- c(0.05, 2.00)
+  ylim <- c(-2, max(unlist(w)))
+
+  ## TODO: Add an inset showing the disruptive selection in top panel.
+  par(mfrow=c(2, 1), mar=c(1.1, 4.1, .5, .5), oma=c(3.1, 0, 0, 0))
+  plot(lma, w$first, type="l", lty=2,
+       log="x", xlim=xlim, ylim=ylim, ylab="Fitness", las=1, xaxt="n")
+  label_panel(1)
+  axis(1, labels=FALSE)
+  lines(lma, w$attractor1)
+  abline(h=0, col="grey")
+  points(communities$first, 0, pch=1)
+  points(communities$attractor1, 0, pch=19)
+
+  i <- lma > 0.1
+  plot(lma[i], w$second1[i], type="l", lty=2,
+       log="x", xlim=xlim, ylim=ylim, ylab="Fitness", las=1)
+  label_panel(2)
+  mtext("Leaf mass per unit area (lma; g / m2)", 1, 3, cex=1)
+  lines(lma[i], w$second2[i], lty=3)
+  lines(lma[i], w$second3[i], lty=3)
+  lines(lma, w$attractor2)
+  abline(h=0)
+  points(communities$attractor2[[1]], 0, pch=19)
+  points(communities$attractor2[[2]], 0, pch=19)
+  points(communities$second1[[2]], 0)
+  points(communities$second2[[2]], 0, col="grey")
+  points(communities$second3[[2]], 0, col="grey")
+}
