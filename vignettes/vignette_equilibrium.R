dcf0c6878c6889ae08753ba5328b0ef25ab4ce95
Rich FitzJohn <rich.fitzjohn@gmail.com>
2015-05-21 17:02:19 +1000

    Fix the vector diagram

--- vignettes/vignette_equilibrium.R
+++ vignettes/vignette_equilibrium.R
@@ -27,10 +27,15 @@ run(1.0, p)
 run(10.0, p)
 run(50.0, p)
 
+## When below the equilibrium, `run` returns a seed rain that is
+## greater than the input, when above it, `run` returns a seed rain
+## that is less than the input.
+
 ## # 1: Approach to equilibrium:
 p_eq <- equilibrium_seed_rain(p)
 
-## Equilibrium seed rain
+## Equilibrium seed rain is right around where the last two values
+## from run were producing
 p_eq$seed_rain
 
 ## From a distance, these both hone in nicely on the equilibrium, and
@@ -38,13 +43,6 @@ p_eq$seed_rain
 ##+ equilibrium_approach
 approach <- attr(p_eq, "progress")
 r <- range(approach)
-plot(approach, type="n", las=1, xlim=r, ylim=r)
-abline(0, 1, lty=2, col="grey")
-cobweb(approach, pch=19, cex=.5, type="o")
-
-## Zoom in on the last few points:
-##+ equilibrium_approach_detail
-r <- p_eq$seed_rain + c(-1, 1) * 0.03
 plot(approach, type="n", las=1, xlim=r, ylim=r)
 abline(0, 1, lty=2, col="grey")
 cobweb(approach, pch=19, cex=.5, type="o")
@@ -61,7 +59,7 @@ seed_rain_out <- unlist(mclapply(seed_rain_in, run, p_eq))
 ## Here is input seeds vs. output seeds:
 ##+ seeds_in_seeds_out
 plot(seed_rain_in, seed_rain_out, xlab="Incoming seed rain",
-     ylab="Outgoing seed rain", las=1, type="l", asp=5)
+     ylab="Outgoing seed rain", las=1, type="l", asp=5, col="red")
 abline(0, 1, lty=2, col="grey")
 cobweb(approach)
 
@@ -81,11 +79,11 @@ plot(seed_rain_in_global, seed_rain_out_global,
      las=1, type="l",
      xlab="Incoming seed rain", ylab="Outgoing seed rain")
 abline(0, 1, lty=2, col="grey")
-abline(fit,  lty=2)
 cobweb(approach, lty=3)
 
 ## # 5. Multiple species at once:
-p2 <- expand_parameters(trait_matrix(c(0.08, 0.15), "lma"), p0, FALSE)
+lma <- c(0.08, 0.15)
+p2 <- expand_parameters(trait_matrix(lma, "lma"), p0, FALSE)
 
 p2_eq <- equilibrium_seed_rain(p2)
 ## TODO: This is not the correct format here:
@@ -103,31 +101,73 @@ for (i in 1:2) {
 }
 abline(v=p2_eq$seed_rain, col=1:2, lty=3)
 
-## Vector field showing the overall appoach
-n <- seq(0.001, max(approach), length.out=21)
-nn <- as.matrix(expand.grid(n, n))
+## Note that the first guess position of the red species is higher
+## than the black species, but in the end the output seed rain is
+## lower.  This is the difficulty in computing multispecies equilibria
+## - the different solutions affect each other.  In general
+## multidimenensional root finding is difficult; even knowning that
+## there are roots is not straightforward, much less proving that
+## we've converged on the "correct" root (for example [0,0] is a root
+## in this case but that root is not stable).  tree uses some
+## heuristics to try to ensure that the root returned is an attracting
+## point but sequentially applying rounds of iteration and nonlinear
+## root finding algorithms, as well as rescaling seed rains to repel
+## from known unstable roots.
 
-x_in  <- unname(split(nn, seq_len(nrow(nn))))
-x_out <- mclapply(x_in, run, p2_eq)
+## To illustrate this a little further, though still in the fairly
+## trivial 2d case, first identify the other two equilibria.
+pp <- mclapply(lma, function(x)
+  equilibrium_seed_rain(expand_parameters(trait_matrix(x, "lma"), p0, FALSE)))
 
-xx <- do.call("rbind", x_out, quote=TRUE)
-r <- diff(n[1:2])
-len <- rowSums(sqrt((xx - nn)^2))
-llen <- log(len)
-rlen <- llen / max(llen) * r * 0.8
+## Here's the seed rains of each species when alone:
+seed_rain1 <- sapply(pp, function(x) x$seed_rain)
 
-theta <- atan2(xx[, 2] - nn[, 2], xx[, 1] - nn[, 1])
+## So that means that we have *four* equilibria:
+## 1: The trivial equilibrium:
+eq00 <- c(0, 0)
+run(eq00, p2_eq) - eq00
 
-x1 <- cbind(nn[, 1] + rlen * cos(theta),
-            nn[, 2] + rlen * sin(theta))
+## 2: Species 1 alone
+eq10 <- c(seed_rain1[[1]], 0)
+run_new_schedule(eq10, p2) - eq10
 
-lim <- range(n) + c(-1, 1) * 0.5 * r
+## 3: Species 2 alone
+eq01 <- c(0, seed_rain1[[2]])
+run_new_schedule(eq01, p2) - eq01
 
-plot(NA, xlim=lim, ylim=lim, xlab="n1", ylab="n2")
-points(nn[, 1], nn[, 2], cex=.25, col="grey")
-arrows(nn[, 1], nn[, 2], x1[, 1], x1[, 2], length=0.02)
-lines(rbind(approach[1, 1:2], approach[, 3:4]), type="o", col="red")
+## 4: Species 1 and 2 together:
+eq11 <- p2_eq$seed_rain
+run(eq11, p2_eq) - eq11
+
+## (note that the approximations here mean that these equilibria are
+## not terribly well polished - there are a set of nested
+## approximations that make this difficult.  Possibly the biggest
+## culprit is the cohort refinement step).
+
+len <- 21
+dx <- max(seed_rain1) / (len - 1)
+n1 <- seq(0.001, by=dx, to=seed_rain1[[1]] + dx)
+n2 <- seq(0.001, by=dx, to=seed_rain1[[2]] + dx)
+nn_in <- as.matrix(expand.grid(n1, n2))
+tmp <- mclapply(unname(split(nn_in, seq_len(nrow(nn_in)))),
+                run_new_schedule, p2_eq)
+nn_out <- do.call("rbind", tmp)
+
+len <- log(rowSums(sqrt((nn_out - nn_in)^2)))
+rlen <- len / max(len) * dx * 0.8
+
+theta <- atan2(nn_out[, 2] - nn_in[, 2], nn_out[, 1] - nn_in[, 1])
+
+x1 <- nn_in[, 1] + rlen * cos(theta)
+y1 <- nn_in[, 2] + rlen * sin(theta)
+
+## NOTE: I'm not sure why the point really close to the equilibrium
+## here looks like it's moving so quickly, and possibly in the wrong
+## direction.  Cohort instability perhaps?
+plot(nn_in, xlab="Species 1", ylab="Species 2", pch=19, cex=.25,
+     col="grey", asp=1)
+arrows(nn_in[, 1], nn_in[, 2], x1, y1, length=0.02)
+lines(rbind(approach[1, 1:2], approach[, 3:4]), type="o", col="red",
+      pch=19, cex=.5)
 points(p2_eq$seed_rain[[1]], p2_eq$seed_rain[[2]], pch=19)
-
-## TODO: Add the unstable equilibria here, which means running this
-## with greater single-species seed rains.
+points(rbind(eq00, eq10, eq01))
