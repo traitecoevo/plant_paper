7831cc684185fe52c8c5833b17edff7bd3318f37
Rich FitzJohn <rich.fitzjohn@gmail.com>
2015-05-27 14:21:07 +1000

    Move from make to remake for ms build

--- R/ms.R
+++ R/ms.R
@@ -33,3 +33,73 @@ pandoc_build <- function(file, template=NULL, format="pdf", ...) {
 ms_deps_timestamp <- function(filename) {
   writeLines(as.character(Sys.time()), filename)
 }
+
+## TODO: move this into callr
+run_system <- function(command, args, env=character()) {
+  res <- suppressWarnings(system2(command, args,
+                                  env=env, stdout=TRUE, stderr=TRUE))
+  ok <- attr(res, "status")
+  if (!is.null(ok) && ok != 0) {
+    cmd <- paste(c(env, shQuote(command), args), collapse = " ")
+
+    msg <- sprintf("Running command:\n  %s\nhad status %d", cmd, ok)
+    errmsg <- attr(cmd, "errmsg")
+    if (!is.null(errmsg)) {
+      msg <- c(msg, sprintf("%s\nerrmsg: %s", errmsg))
+    }
+
+    sep <- paste(rep("-", getOption("width")), collapse="")
+    msg <- c(msg, "Program output:", sep, res, sep)
+    stop(paste(msg, collapse="\n"))
+  }
+  invisible(res)
+}
+
+## TODO: move this into remake
+## TODO: options for saying what command is being run
+latex_build <- function(filename, bibliography=NULL,
+                        chdir=TRUE, interaction="nonstopmode",
+                        max_attempts=5L, clean=FALSE) {
+  latex <- Sys.which("pdflatex")
+  if (latex == "") {
+    stop("latex not found in $PATH")
+  }
+
+  if (chdir && dirname(filename) != "") {
+    owd <- setwd(dirname(filename))
+    on.exit(setwd(owd))
+    filename <- basename(filename)
+  }
+
+  args <- c(paste0("-interaction=", interaction),
+            "-halt-on-error",
+            filename)
+  res <- run_system(latex, args)
+  if (!is.null(bibliography)) {
+    run_system(Sys.which("bibtex"), sub(".tex$", "", filename))
+    res <- run_system(latex, args)
+  }
+
+  pat <- "Rerun to get cross-references right"
+  for (i in seq_len(max_attempts)) {
+    if (any(grepl(pat, res))) {
+      res <- run_system(latex, args)
+    } else {
+      break
+    }
+  }
+
+  if (clean) {
+    latex_clean(filename)
+  }
+
+  invisible(NULL)
+}
+
+latex_clean <- function(filename) {
+  filebase <- sub(".tex$", "", filename)
+  exts <- c(".log", ".aux", ".bbl", ".blg", ".fls", ".out", ".snm",
+            ".nav", ".tdo", ".toc")
+  aux <- paste0(filebase, exts)
+  file.remove(aux[file.exists(aux)])
+}
