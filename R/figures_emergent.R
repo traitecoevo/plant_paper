8a245ebd3ae55d48d13ddab11861eb9206dbc4ea
Daniel Falster <daniel.falster@mq.edu.au>
2015-05-28 20:35:24 +1000

    Plot averages with respect to height across metapopulation.
    For #13
    
    Squashed commit of the following:
    
    commit 1d0640e721e018abc5ba7dfc459a45810ebe6172
    Author: Daniel Falster <daniel.falster@mq.edu.au>
    Date:   Thu May 28 20:34:40 2015 +1000
    
        Increase resolution
    
    commit d8df1369e081494d722500912f395b5e02079c65
    Author: Daniel Falster <daniel.falster@mq.edu.au>
    Date:   Thu May 28 18:56:47 2015 +1000
    
        interpolate using spline_loglog
    
    commit 85cec05505f81e0f17f692dd60d5fc98b659b9e2
    Author: Daniel Falster <daniel.falster@mq.edu.au>
    Date:   Thu May 28 17:09:50 2015 +1000
    
        First cut at getting averages with respect to height across metapopulation
    
        For #13
    
        Lines too noisy to use

--- R/figures_emergent.R
+++ R/figures_emergent.R
@@ -1,5 +1,10 @@
-make_emergent_data <- function() {
+make_emergent_data <- function(hires=TRUE) {
   p0 <- ebt_base_parameters()
+  if(hires) {
+    p0$control$schedule_eps <- 2e-6
+    p0$control$ode_tol_rel <- 1e-6
+    p0$control$ode_tol_abs <- 1e-6
+  }
   p0$disturbance_mean_interval <- 30.0
   p1 <- expand_parameters(trait_matrix(0.08, "lma"), p0, FALSE)
   p1 <- build_schedule(p1)
@@ -13,12 +18,22 @@ figure_emergent <- function(data) {
   last <- function(x) {
     x[[length(x)]]
   }
+  splinefun_loglog2 <- function(x,y,xx){
+    ret <- splinefun_loglog(x,y)(xx)
+    ii <- xx <= max(x, na.rm=TRUE) & xx >= min(x, na.rm=TRUE) 
+    ret[!ii] <-0 
+    ret
+  }
 
   species <- data$species[[1]]
   height <- t(species["height", , ])
   log_density <- t(species["log_density", , ])
   density <- exp(log_density)
 
+  # times for background grey lines
+  j <- seq(1, length(data$time), by=10)
+
+  # time to highlight in blue
   times <- c(5, 10, 20, 40, last(data$time))
   i <- vapply(times, closest, integer(1), data$time)
   blues <- c("#DEEBF7", "#C6DBEF", "#9ECAE1", "#6BAED6",
@@ -34,11 +49,37 @@ figure_emergent <- function(data) {
   xxx <- lapply(tmp, function(x) matrix(x$species[[1]]$ode_rates, 4)[1, ])
   growth_rate <- do.call("cbind", plant:::pad_matrix(xxx))
 
-  op <- par(mfrow=c(3, 1), mar=c(2.1, 4.1, .5, .5), oma=c(2, 0, 0, 0))
+  # interpolate density in each patch to common set of heights
+  hh <- seq_log_range(range(height, na.rm=TRUE), 500)
+  xxx <- lapply(seq_along(data$time), function(i) 
+              splinefun_loglog2(height[,i],density[,i], hh))
+  n_hh <- do.call("cbind", plant:::pad_matrix(xxx))
+
+  # interpolate leaf area in each patch to common set of heights
+  xxx <- lapply(seq_along(data$time), function(i) 
+              splinefun_loglog2(height[,i], leaf_area[,i], hh))
+  l_hh <- do.call("cbind", plant:::pad_matrix(xxx))
+  
+  # interpolate growth_rate in each patch to common set of heights
+  xxx <- lapply(seq_along(data$time), function(i) 
+              splinefun_loglog2(height[,i],growth_rate[,i], hh))
+  g_hh <- do.call("cbind", plant:::pad_matrix(xxx))
+ 
+  # find average across patches for each height, 
+  # i.e. across rows, weighting by patch abundance 
+  metapopulation_average <- function(x) {
+    tree:::trapezium(data$time, x*data$patch_density)
+  }  
+  n_av <- apply(n_hh,      1, metapopulation_average) 
+  l_av <- apply(l_hh*n_hh, 1, metapopulation_average) / n_av
+  g_av <- apply(g_hh*n_hh, 1, metapopulation_average) / n_av
+ 
+  op <- par(mfrow=c(3, 1), mar=c(2.1, 4.1, .5, .5), oma=c(2, 0.5, 0, 0))
   on.exit(par(op))
+
   ## First panel: density vs height:
   xlim <- c(0, max(height, na.rm=TRUE) * 1.05)
-  matplot(height, density, type="l", lty=1, col="lightgrey",
+  matplot(height[, j], density[, j], type="l", lty=1, col="lightgrey",
           xlim=xlim, xlab="Height (m)", ylab="Density (1 / m / m2)", las=1,
           xaxt="n", log="y")
   axis(1, labels=FALSE)
@@ -48,8 +89,9 @@ figure_emergent <- function(data) {
        paste0(round(times), c(" years", rep("", length(times) - 1))),
        adj=c(0, 0))
   label_panel(1)
+  points(hh, n_av, col="red", type='l')
 
-  matplot(height, leaf_area, type="l", lty=1, col="lightgrey",
+  matplot(height[, j], leaf_area[, j], type="l", lty=1, col="lightgrey",
           xlim=xlim, xlab="Height (m)",
           ylab="Leaf area density (m2 / m2 / m)", las=1,
           xaxt="n")
@@ -60,8 +102,9 @@ figure_emergent <- function(data) {
        paste0(round(times), c(" years", rep("", length(times) - 1))),
        adj=c(0, 0))
   label_panel(2)
+  points(hh, l_av, col="red", type='l')
 
-  matplot(height, growth_rate, type="l", lty=1, col="lightgrey",
+  matplot(height[, j], growth_rate[, j], type="l", lty=1, col="lightgrey",
           xlim=xlim, xlab="Height (m)",
           ylab="Height growth rate (m / year)", las=1)
   matlines(height[, i], growth_rate[, i], col=cols, lty=1, type="l")
@@ -71,4 +114,6 @@ figure_emergent <- function(data) {
        adj=c(0, 0))
   label_panel(3)
   mtext("Height (m)", 1, 3, xpd=NA, cex=.75)
+  points(hh, g_av, col="red", type='l')
+
 }
