d14b7eec7cd2a070b3890452ead1351d72d5d656
Rich FitzJohn <rich.fitzjohn@gmail.com>
2015-06-01 16:10:11 +1000

    Update to the release generation

--- release.sh
+++ release.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 set -e
 remake all
-rm -rf plant_paper
+rm -rf release supporting_material plant_paper
+mkdir release
+cp ms.pdf release
+
+mkdir supporting_material
+cp vignettes/*.html vignettes/*.pdf supporting_material
+zip -r release/supporting_material.zip supporting_material
+rm -rf supporting_material
+
+## All artefacts:
 mkdir plant_paper
-cp -rp vignettes figures ms.pdf plant_paper
-zip -r plant_paper.zip plant_paper
+cp -r figures vignettes ms.pdf plant_paper
+zip -r release/plant_paper.zip plant_paper
 rm -rf plant_paper
