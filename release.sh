8d6da374d0c87bb1e4ba9bb3db737db672afbd0b
Rich FitzJohn <rich.fitzjohn@gmail.com>
2015-06-01 18:04:18 +1000

    Work around manuscript central :fire::fire:

--- release.sh
+++ release.sh
@@ -6,7 +6,10 @@ mkdir release
 cp ms.pdf release
 
 mkdir supporting_material
-cp vignettes/*.html vignettes/*.pdf supporting_material
+for file in $(find vignettes -name '*.html' | sed -e 's|.*/||'); do
+    echo "Converting ${file}"
+    (cd vignettes; pandoc "$file" -o ../supporting_material/"${file%.html}.pdf" --template template.tex)
+done
 zip -r release/supporting_material.zip supporting_material
 rm -rf supporting_material
 
